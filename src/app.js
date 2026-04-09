8454dfbd3fab3355976b2b6e218719f83a36b3f4
Krishnabisht07 <bishtkrishna7094@gmail.com>
2026-04-10 00:02:19 +0530

    handling multiple handlers ep5

--- src/app.js
+++ src/app.js
@@ -17,6 +17,28 @@ app.listen(3000, () => {
 //     res.send('<h1>Hello World</h1>');
 // });
 //below code means server is listening at port 3000 but it will only respond when the route is home.
-app.use("/home", (req, res) => {
-    res.send('<h1>Welcome to Home Page 1</h1>');
-});
+// app.use("/", (req, res) => {
+//     res.send('<h1>Welcome to Home</h1>');
+// });
+// app.use("/home/12", (req, res) => {
+//     res.send('<h1>Welcome to Home 12</h1>');
+// });
+// app.use("/home", (req, res) => {
+//     res.send('<h1>Welcome to Home Page 1</h1>');
+// });
+
+// app.use("/test", (req, res) => {
+//     res.send('<h1>Welcome to Test Page</h1>');
+// });
+app.get("/get-home",[(req,res,next)=>{
+   console.log("response from first handler");
+   next();
+  
+},(req,res,next)=>{
+    console.log("response from second handler");
+    res.send("hi from today...")
+}])
+// app.post("/get-home",(req,res)=>{
+//     res.send("This is from post http method");
+//     console.log(req.query);
+// })
